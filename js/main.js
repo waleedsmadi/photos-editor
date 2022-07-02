f9b58ffba3d26e832eeb243b222eac897032ff89
waleed smadi <waleed0smadi@outlook.com>
2022-07-02 13:40:43 +0300

    Select all elements JS

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// selectors
+const saturate = document.querySelector("#saturate");
+const contrast = document.querySelector("#contrast");
+const brightness = document.querySelector("#brightness");
+const sepia = document.querySelector("#sepia");
+const grayscale = document.querySelector("#grayscale");
+const blur = document.querySelector("#blur");
+const hueRotate = document.querySelector("#hue-rotate");
+
+
+const upload = document.querySelector("#upload");
+const download = document.querySelector("#download");
+const reset = document.querySelector("#reset")
+
+const photo = document.querySelector("#img");
+const imgBox = document.querySelector(".img-box");
+
+
